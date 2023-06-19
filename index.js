6dd0eb90257408c16ab4accdfdf1f52c86cab08c
Linet <muthiigathoni@gmail.com>
2023-06-19 08:16:52 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
+
 // Code your solutions in this file
+/*
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    debugger;
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
+*/
+
+const name = ["Guadalupe", "Ollie", "Aki"]
+function writeCards(name, surprise){
+  let invitations = [];
+  for (let i=0; i < name.length; i++){
+    invitations.push(`Thank you, ${name[i]}, for the wonderful ${surprise} gift!`);  
+  }
+  return invitations;
+}
+console.log(writeCards());
+
+function countDown(){
+  let num = 10;
+  while(num >= 0){
+    console.log(num);
+    num = num -1;
+  }
+}  
+countDown()
